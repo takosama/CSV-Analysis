0c545e85559be0f3887fcb11c1cb6007061a933d
takosama <takosama.312@gmail.com>
2018-11-29 21:07:05 +0900

    Update main.cs

--- main.cs
+++ main.cs
@@ -8,6 +8,8 @@ namespace ConsoleApp35
     public unsafe class MyStr
     {
         volatile string str = "aaaa,123,1234,bbbbbbbbbbbbbbbbbbbasdaadsaf0010bbbbbbbbbbbbbbbbbbbbbasdaadsaf0010bb,0\r\naaaab,1234,12345,abbbb,1\n";
+
+        //index が入るはずの配列
         volatile int[] tmp = new int[100];
         //99
         [BenchmarkDotNet.Attributes.Benchmark]
@@ -28,6 +30,25 @@ namespace ConsoleApp35
             }
         }
         //24
+     
+
+
+
+        unsafe int GetControlIndex(ReadOnlySpan<char> c, int index)
+        {
+            int i = 0;
+            while (true)
+            {
+                int pos = i + index;
+
+                if (c.Length == pos)
+                    return -1;
+                if (c[pos] == ',' || c[pos] == '\n')
+                    return i;
+                i++;
+            }
+        }
+
         [BenchmarkDotNet.Attributes.Benchmark]
         public void GetControlIndexSIMD()
         {
@@ -51,33 +72,16 @@ namespace ConsoleApp35
         }
 
 
-
-        unsafe int GetControlIndex(ReadOnlySpan<char> c, int index)
-        {
-            int i = 0;
-            while (true)
-            {
-                int pos = i + index;
-
-                if (c.Length == pos)
-                    return -1;
-                if (c[pos] == ',' || c[pos] == '\n')
-                    return i;
-                i++;
-            }
-        }
-
         Vector128<sbyte> _koron = Sse2.SetAllVector128((sbyte)',');
         Vector128<sbyte> _lf = Sse2.SetAllVector128((sbyte)'\n');
         Vector128<sbyte> _0 = Sse2.SetAllVector128((sbyte)'\0');
 
         Vector128<sbyte> maskMove0 = Sse2.SetVector128(-1, -1, -1, -1, -1, -1, -1, -1, 14, 12, 10, 8, 6, 4, 2, 0);
-        Vector128<sbyte> maskMove1 = Sse2.SetVector128(14, 12, 10, 8, 6, 4, 2, 0, -1, -1, -1, -1, -1, -1, -1, -1);
-        //24ns
+
         unsafe int GetControlIndexSIMD(sbyte* c)
         {
             int cnt = 0;
-        start:
+            start:
             var str = Sse2.LoadVector128(c);
 
             str = Ssse3.Shuffle(str, maskMove0);
